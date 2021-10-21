1bb952ed3b7a5a7263234769aa8e9155671c98e1
Caio Cesar <caiocesaraguilar14@hotmail.com>
2021-10-21 17:50:36 -0300

    adicionado dropdown menu para escolha da subestação utilizando JSON

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,16 +1,35 @@
+import React, { Component } from 'react';
+import * as data from './data.json';
+
 const Main = () => {
+    const [value, setValue] = React.useState();
     return (
         <div
             style={{
                 height: '100vh',
                 width: '100%',
                 background: 'lightgreen',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
+                justifyContent: 'center'
             }}
         >
-            Página principal
+            <select 
+                value={value} 
+                onChange={e=>setValue(e.currentTarget.value)}
+            >
+                {data.default.map((item,index) => ( // É necessário acessar default pois ao import para varíavel data é criada uma instância superior denominada default
+                    <option
+                    key={index}
+                    value={item.nome}
+                    >
+                    {item.nome}
+                    </option>
+                ))}
+            </select>
+            <p>
+                {value}
+            </p>
+
+            
 
         </div>
     );
--- src/Pages/Simula.jsx
+++ src/Pages/Simula.jsx
@@ -1,9 +1,8 @@
 import '../index.css';
 import React, { Component } from 'react';
-
+import * as data from './data.json';
 
 function Disjuntor (props) {
-   
     return (
       <button 
         className="disjuntor" 
@@ -21,6 +20,7 @@ function Disjuntor (props) {
 class Diagrama extends React.Component {
   
     renderDisjuntor(x,y,index) {
+        
       return <Disjuntor 
                 x={x} // envia coordenadas x e y para renderizar disjuntor
                 y={y}
@@ -29,17 +29,21 @@ class Diagrama extends React.Component {
     }
   
     render() {
+        
         const mapa = JSON.parse(this.props.v); // (des)converte de JSON para objeto
+        
         return(
+            
             mapa.map((dis,index)=>{ // cria loop em que se lê todos os disjuntores chamando a função de renderização para cada um deles
                 return (
-                    <div>
-                        {this.renderDisjuntor(dis.pos[0],dis.pos[1],index)}
+                    <div key={index}>
+                        {this.renderDisjuntor(dis[0],dis[1],index)}
                     </div>
                     
                 );
             })
         );
+        
     }
   }
 
@@ -50,12 +54,8 @@ export default class Simula extends Component {
         this.state = {
           selectValue: "Betim4",
           x: 500,
-          mapa: [  // mapa de disjuntores em pixels de distancia
-              {pos: [30,32]},
-              {pos: [241,142]},
-              {pos: [171,272]},
-              {pos: [391,392]}
-        ]
+          
+          mapa: [...data[0].mapa]
         };
     
         this.handleDropdownChange = this.handleDropdownChange.bind(this);
@@ -65,7 +65,12 @@ export default class Simula extends Component {
       }
 
     render() {
+
         const mapa = this.state.mapa;
+
+        const word = mapa[1][1];
+        console.log(word); // output 'testing'
+        //const data = JSON.parse(myJson)
         return (
             <div
                 style={{
@@ -79,17 +84,7 @@ export default class Simula extends Component {
                 fontWeight: 'bold'
             }}
         >
-            <div>
-                <div>
-                    <div>
-                        <select id="dropdown" onChange={this.handleDropdownChange}>
-                        <option value="Betim4">Betim 4</option>
-                        <option value="Cicontagem">CI Contagem</option>
-                        <option value="Centro2">Centro 2</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
+            
 
             <p>Simula Subestação</p>
             
@@ -103,7 +98,7 @@ export default class Simula extends Component {
                     position: 'relative'
                 }}>
                     <h1>Hello</h1>
-                    <p>{mapa[1].pos[1]}</p>
+                    <p>{mapa[1][1]}</p>
                     <Diagrama 
                         v={JSON.stringify(mapa)} //Necessário conversão para JSON pois objetos não podem ser passados em props
                     />
